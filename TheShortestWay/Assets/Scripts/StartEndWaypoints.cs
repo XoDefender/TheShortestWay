f27647ef55901e7e0a65c9841595a2dca316278a
XoDefender <kir.zhilnikov37@internet.ru>
2022-05-31 09:40:39 +0300

    Add water

--- TheShortestWay/Assets/Scripts/StartEndWaypoints.cs
+++ TheShortestWay/Assets/Scripts/StartEndWaypoints.cs
@@ -71,7 +71,7 @@ public class StartEndWaypoints : MonoBehaviour
 
             if (Input.GetMouseButtonDown(0))
             {
-                if (Physics.Raycast(ray, out hit, 100, waypointLayerMask))
+                if (Physics.Raycast(ray, out hit, 1000, waypointLayerMask))
                     hasPickedTargetWaypoint = true;
             }
         }
