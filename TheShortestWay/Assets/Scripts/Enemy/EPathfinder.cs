98858e49b51a503adc901ab281ee2d86bd3258a1
XoDefender <kir.zhilnikov37@internet.ru>
2022-06-03 11:16:12 +0300

    Explore the paths to each waypoint (Enemy)

--- TheShortestWay/Assets/Scripts/Enemy/EPathfinder.cs
+++ TheShortestWay/Assets/Scripts/Enemy/EPathfinder.cs
@@ -4,15 +4,132 @@ using UnityEngine;
 
 public class EPathfinder : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private EStartTargetWaypoints startTargetWaypoints;
+    private EWaypointData currentlyGoingFrom;
+    private EWaypointData[] waypoints;
+
+    private Dictionary<Vector2Int, EWaypointData> roadWaypoints = new Dictionary<Vector2Int, EWaypointData>();
+    private Dictionary<Vector2Int, EWaypointData> toFrom = new Dictionary<Vector2Int, EWaypointData>();
+
+    private Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.down,
+        Vector2Int.left,
+        Vector2Int.right
+    };
+
+    private Queue<EWaypointData> exploringWaypoints = new Queue<EWaypointData>();
+    private List<EWaypointData> exploredWaypoints = new List<EWaypointData>();
+    private List<EWaypointData> path = new List<EWaypointData>();
+
+    private bool readyToFindPath = false;
+    private bool isStartWaypointInQueue = false;
+    private bool isObserved = false;
+
+    private void Awake()
     {
-        
+        startTargetWaypoints = GetComponent<EStartTargetWaypoints>();
+        waypoints = FindObjectsOfType<EWaypointData>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        foreach (EWaypointData waypoint in waypoints)
+        {
+            if (!roadWaypoints.ContainsKey(waypoint.GetGridPosition()))
+                roadWaypoints.Add(waypoint.GetGridPosition(), waypoint);
+        }
+    }
+
     void Update()
     {
+        BreadthFirstSearch(startTargetWaypoints.StartWaypoint, startTargetWaypoints.ReadyToPickTargetWaypoint);
+    }
+
+    private void BreadthFirstSearch(EWaypointData startWaypoint, bool readyToPickTargetWaypoint)
+    {
+        if (startWaypoint && !readyToPickTargetWaypoint)
+        {
+            if (!isStartWaypointInQueue)
+            {
+                exploringWaypoints.Enqueue(startTargetWaypoints.StartWaypoint);
+                isStartWaypointInQueue = true;
+            }
+
+            if (!path.Contains(startTargetWaypoints.StartWaypoint))
+                exploredWaypoints.Add(startTargetWaypoints.StartWaypoint);
+
+            if (!path.Contains(startTargetWaypoints.TargetWaypoint))
+                path.Add(startTargetWaypoints.TargetWaypoint);
+
+            currentlyGoingFrom = startTargetWaypoints.TargetWaypoint;
+
+            foreach (Vector2Int direction in directions)
+            {
+                if (exploringWaypoints.Count != 0)
+                {
+                    Vector2Int exploredWaypointCoordinates = exploringWaypoints.Peek().GetGridPosition() + direction;
+
+                    if (roadWaypoints.ContainsKey(exploredWaypointCoordinates))
+                    {
+                        if (!exploredWaypoints.Contains(roadWaypoints[exploredWaypointCoordinates]) && !exploringWaypoints.Contains(roadWaypoints[exploredWaypointCoordinates]))
+                        {
+                            exploringWaypoints.Enqueue(roadWaypoints[exploredWaypointCoordinates]);
+                            exploredWaypoints.Add(roadWaypoints[exploredWaypointCoordinates]);
+                            toFrom.Add(exploredWaypointCoordinates, exploringWaypoints.Peek());
+                        }
+                    }
+                }
+            }
+
+            if (exploringWaypoints.Count != 0)
+            {
+                exploringWaypoints.Dequeue();
+                readyToFindPath = false;
+            }
+            else
+                readyToFindPath = true;
+
+            if (readyToFindPath)
+            {
+                if (toFrom.ContainsKey(currentlyGoingFrom.GetGridPosition()))
+                {
+                    while (toFrom[currentlyGoingFrom.GetGridPosition()] != startTargetWaypoints.StartWaypoint)
+                    {
+                        path.Add(toFrom[currentlyGoingFrom.GetGridPosition()]);
+                        currentlyGoingFrom = toFrom[currentlyGoingFrom.GetGridPosition()];
+                    }
+                }
+
+                if (!path.Contains(startTargetWaypoints.StartWaypoint))
+                    path.Add(startTargetWaypoints.StartWaypoint);
+
+                path.Reverse();
+
+                startTargetWaypoints.ReadyToPickTargetWaypoint = true;
+                isObserved = true;
+
+                foreach (EWaypointData waypoint in path)
+                {
+                    waypoint.GetComponent<MeshRenderer>().material.color = Color.blue;
+                }
+            }
+        }
         
+        if(isObserved)
+        {
+            DataReset();
+            isObserved = false;
+        }
+    }
+
+    public void DataReset()
+    {
+        path.Clear();
+        exploredWaypoints.Clear();
+        toFrom.Clear();
+
+        isStartWaypointInQueue = false;
+        readyToFindPath = false;
     }
 }
--- TheShortestWay/Assets/Scripts/Enemy/EStartTargetWaypoints.cs
+++ TheShortestWay/Assets/Scripts/Enemy/EStartTargetWaypoints.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class EStartTargetWaypoints : MonoBehaviour
 {
     private EWaypointData[] waypoints;
+    private EWaypointData startWaypoint;
+    private EWaypointData targetWaypoint;
+    private GameObject enemy;
+
+    private const string enemyName = "Enemy";
 
     private bool readyToPickTargetWaypoint = true;
     private int targetWaypointNumber = 0;
@@ -12,16 +17,48 @@ public class EStartTargetWaypoints : MonoBehaviour
     private void Awake()
     {
         waypoints = FindObjectsOfType<EWaypointData>();
+        enemy = GameObject.Find(enemyName);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(readyToPickTargetWaypoint && targetWaypointNumber < waypoints.Length)
+        PickStartWaypoint(waypoints);
+        PickTargetWaypoint();
+    }
+
+    private void PickStartWaypoint(EWaypointData[] waypoints)
+    {
+        if (startWaypoint == null)
         {
+            foreach (EWaypointData waypoint in waypoints)
+            {
+                if (Mathf.Approximately(waypoint.transform.position.x, enemy.transform.position.x) && Mathf.Approximately(waypoint.transform.position.z, enemy.transform.position.z))
+                {
+                    startWaypoint = waypoint;
+
+                    break;
+                }
+            }
+        }
+    }
+
+    private void PickTargetWaypoint()
+    {
+        if (readyToPickTargetWaypoint && targetWaypointNumber < waypoints.Length)
+        {
+            targetWaypoint = waypoints[targetWaypointNumber];
+
             waypoints[targetWaypointNumber].SetColor(Color.red);
 
             targetWaypointNumber += 1;
+
+            readyToPickTargetWaypoint = false;
         }
     }
+
+    public EWaypointData StartWaypoint { get { return startWaypoint; } set { startWaypoint = value; } }
+    public EWaypointData TargetWaypoint { get { return targetWaypoint; } }
+
+    public bool ReadyToPickTargetWaypoint { get { return readyToPickTargetWaypoint; } set { readyToPickTargetWaypoint = value; } }
 }
